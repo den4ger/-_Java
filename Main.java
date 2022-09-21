4fc15b62897b04e45892e03d5955f1f9ea076116
den4ger <114094217+den4ger@users.noreply.github.com>
2022-09-21 18:54:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String art = s.nextLine();
+        System.out.println("zalupa "+art);
+    }
+}
